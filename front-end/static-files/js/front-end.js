ae57ecf6abcb7004f5d75cd873e046b832111cd7
Francisco Javier Jiménez Aznar <124682845+fjja0004@users.noreply.github.com>
2023-04-29 18:25:50 +0200

    Actualización front-end.js
    Se han añadido dos funciones para mostrar y ocultar los botones correspondientes a la aplicación de críquet.

--- front-end/static-files/js/front-end.js
+++ front-end/static-files/js/front-end.js
@@ -9,7 +9,7 @@
 let Frontend = {};
 
 // Estos dos métodos muestran los botones de la aplicación de fútbol sala. Al pulsar sobre el botón 'Home'
-// se vuelve a mostrar el botón con id 'brn-fsala' y es ocultan todos los demás
+// se vuelve a mostrar el botón con id 'btn-fsala' y es ocultan todos los demás
 Frontend.mostrarOpcionesFSala = function (){
     document.getElementById("btn-fsala").classList.remove("mostrar")
     document.getElementById("btn-fsala-listado").classList.remove("ocultar")
@@ -37,6 +37,33 @@ Frontend.ocultarOpcionesFSala = function () {
     document.getElementById("btn-fsala-nombresorden").classList.add("ocultar")
     document.getElementById("btn-fsala-añadir").classList.add("ocultar")
 }
+
+/**
+ * Muestra los botones de la aplicación de críquet. 
+ */
+Frontend.mostrarOpcionesCriquet = function (){
+    document.getElementById("btn-criquet").classList.remove("mostrar")
+    document.getElementById("btn-criquet-listado").classList.remove("ocultar")
+    document.getElementById("btn-criquet-nombres").classList.remove("ocultar")
+
+    document.getElementById("btn-criquet").classList.add("ocultar")
+    document.getElementById("btn-criquet-listado").classList.add("mostrar")
+    document.getElementById("btn-criquet-nombres").classList.add("mostrar")
+}
+
+/**
+ * Al pulsar sobre el botón 'Home' se vuleve a mostrar el botón con id 'btn-criquet' y se ocultan los demás.
+ */
+Frontend.ocultarOpcionesCriquet = function () {
+    document.getElementById("btn-criquet").classList.remove("ocultar")
+    document.getElementById("btn-criquet-listado").classList.remove("mostrar")
+    document.getElementById("btn-criquet-nombres").classList.remove("mostrar")
+
+    document.getElementById("btn-criquet").classList.add("mostrar")
+    document.getElementById("btn-criquet-listado").classList.add("ocultar")
+    document.getElementById("btn-criquet-nombres").classList.add("ocultar")
+}
+
 
 /// Dirección del MS que funciona como API_GATEWAY
 Frontend.API_GATEWAY = "http://localhost:8001"
