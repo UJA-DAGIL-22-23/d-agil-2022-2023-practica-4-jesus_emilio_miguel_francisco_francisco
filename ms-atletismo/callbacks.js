1b5a180095bc98ddc074d0b169e2587dcd2af46f
Emilio Martínez Conchillo <emc00073@red.ujaen.es>
2023-05-03 12:41:24 +0200

    Añadida la carpeta ms-atletismo

---
+++ ms-atletismo/callbacks.js
@@ -0,0 +1,151 @@
+/**
+ * @file callbacks.js
+ * @description Callbacks para el MS Plantilla.
+ * Los callbacks son las funciones que se llaman cada vez que se recibe una petición a través de la API.
+ * Las peticiones se reciben en las rutas definidas en routes.js, pero se procesan aquí.
+ * @author Víctor M. Rivas <vrivas@ujaen.es>
+ * @date 03-feb-2023
+ */
+
+
+
+// Necesario para conectar a la BBDD faunadb
+const faunadb = require('faunadb'),
+    q = faunadb.query;
+
+const client = new faunadb.Client({
+    secret: 'fnAFBM8AoGAAzHhNu7qy_9GkZbYws97wHENaXtVk',
+});
+
+const COLLECTION = "Atletas"
+
+// CALLBACKS DEL MODELO
+
+/**
+ * Función que permite servir llamadas sin importar el origen:
+ * CORS significa Cross-Origin Resource Sharing
+ * Dado un objeto de tipo respuesta, le añade las cabeceras necesarias para realizar CROS
+ * @param {*} res Objeto de tipo response 
+ * @returns Devuelve el mismo objeto para concatenar varias llamadas al mismo
+ */
+function CORS(res) {
+    res.header('Access-Control-Allow-Origin', '*')
+        .header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        )
+    return res;
+}
+
+
+/**
+ * Objeto que contiene las funciones callback para interactuar con el modelo (e.d., la BBDD)
+ */
+const CB_MODEL_SELECTS = {
+    /**
+     * Prueba de conexión a la BBDD: devuelve todas las personas que haya en la BBDD.
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    test_db: async (req, res) => {
+        try {
+            let personas = await client.query(
+                q.Map(
+                    q.Paginate(q.Documents(q.Collection(COLLECTION))),
+                    q.Lambda("X", q.Get(q.Var("X")))
+                )
+            )
+            res.status(200).json(personas)
+        } catch (error) {
+            res.status(500).json({ error: error.description })
+        }
+    },
+
+    /**
+     * Método para obtener todos los atletas de la BBDD.
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    getTodos: async (req, res) => {
+        try {
+            let atletas = await client.query(
+                q.Map(
+                    q.Paginate(q.Documents(q.Collection("Atletas"))),
+                    q.Lambda("X", q.Get(q.Var("X")))
+                )
+            )
+            // console.log( atletas ) // Para comprobar qué se ha devuelto en atletas
+            CORS(res)
+                .status(200)
+                .json(atletas)
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
+
+    /**
+     * Método para obtener un atleta por su identificador de la BBDD.
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    getAtletaPorId: async (req, res) => {
+        try {
+            const id = req.query.id //obtiene el identificador del atleta de la URL
+            const atleta = await client.query(
+                q.Get(q.Ref(q.Collection("Atletas"), id))
+            )
+            // console.log( atleta ) // Para comprobar qué se ha devuelto en atleta
+            CORS(res)
+                .status(200)
+                .json(atleta)
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    }
+}
+
+
+
+// CALLBACKS ADICIONALES
+
+/**
+ * Callbacks adicionales. Fundamentalmente para comprobar que el ms funciona.
+ */
+const CB_OTHERS = {
+    /**
+     * Devuelve un mensaje indicando que se ha accedido a la home del microservicio
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    home: async (req, res) => {
+        try {
+            CORS(res).status(200).json({ mensaje: "Microservicio MS Plantilla: home" });
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
+
+    /**
+     * Devuelve un mensaje indicando que se ha accedido a la información Acerca De del microservicio
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    acercaDe: async (req, res) => {
+        try {
+            CORS(res).status(200).json({
+                mensaje: "Microservicio MS Plantilla: acerca de",
+                autor: "Emilio Martínez Conchillo",
+                email: "emc00073@red.ujaen.es",
+                fecha: "10/04/2023"
+            });
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
+
+}
+
+// Une todos los callbacks en un solo objeto para poder exportarlos.
+// MUY IMPORTANTE: No debe haber callbacks con el mismo nombre en los distintos objetos, porque si no
+//                 el último que haya SOBREESCRIBE a todos los anteriores.
+exports.callbacks = { ...CB_MODEL_SELECTS, ...CB_OTHERS }
