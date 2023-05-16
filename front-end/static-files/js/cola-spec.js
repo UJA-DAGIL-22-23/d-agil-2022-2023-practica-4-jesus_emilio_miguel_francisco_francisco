e8fd7b85c0dbf1e948866550b35cebf2d7bb51ff
mahm0010 <mahm0010@red.ujaen.es>
2023-05-16 13:54:58 +0200

    Añado dos nuevas pruebas para cola
    Añado las pruebas correspondientes a las funciones obtenerElementos y borrar de la cola

--- front-end/static-files/js/cola-spec.js
+++ front-end/static-files/js/cola-spec.js
@@ -4,9 +4,6 @@
  * @author Miguel Ángel Hurtado Molina <mahm0010@red.ujaen.es>
  */
 
-
-// SPECS a probar
-
 describe('Cola', function () {
     let cola;
   
@@ -14,18 +11,19 @@ describe('Cola', function () {
       cola = new Cola(3);
     });
   
-    it('debería encolar y desencolar correctamente', function () {
-      cola.encolar('Elemento 1');
-      cola.encolar('Elemento 2');
-      console.log("COLA");
-      console.log(cola);
-      expect(cola.desencolar()).toBe('Elemento 1');
-      cola.encolar('Elemento 3');
-      expect(cola.desencolar()).toBe('Elemento 2');
-      expect(cola.desencolar()).toBe('Elemento 3');
+    describe('encolar y desencolar', function () {
+      it('debería encolar y desencolar correctamente', function () {
+        cola.encolar('Elemento 1');
+        cola.encolar('Elemento 2');
+        expect(cola.desencolar()).toBe('Elemento 1');
+        cola.encolar('Elemento 3');
+        expect(cola.desencolar()).toBe('Elemento 2');
+        expect(cola.desencolar()).toBe('Elemento 3');
+      });
     });
 
-    it('debería devolver el tamaño correcto de la cola', function () {
+    describe('tam', function () {
+      it('debería devolver el tamaño correcto de la cola', function () {
         expect(cola.tam()).toBe(0);
         cola.encolar('Elemento 1');
         cola.encolar('Elemento 2');
@@ -33,13 +31,35 @@ describe('Cola', function () {
         cola.desencolar();
         expect(cola.tam()).toBe(1);
       });
+    });
 
-    it('debería verificar si la cola está vacía correctamente', function () {
+    describe('estaVacia', function () {
+      it('debería verificar si la cola está vacía correctamente', function () {
         expect(cola.estaVacia()).toBe(true);
         cola.encolar('Elemento 1');
         expect(cola.estaVacia()).toBe(false);
         cola.desencolar();
         expect(cola.estaVacia()).toBe(true);
+      });
+    });
+
+    describe('obtenerElementos', function () {
+      it('debería devolver un vector con todos los elementos de la cola', function () {
+        cola.encolar('Elemento 1');
+        cola.encolar('Elemento 2');
+        cola.encolar('Elemento 3');
+        expect(cola.obtenerElementos()).toEqual(['Elemento 1', 'Elemento 2', 'Elemento 3']);
+      });
+    });
+
+    describe('borrar', function () {
+      it('debería vaciar la cola', function () {
+        cola.encolar('Elemento 1');
+        cola.encolar('Elemento 2');
+        cola.borrar();
+        expect(cola.estaVacia()).toBe(true);
+        expect(cola.tam()).toBe(0);
+      });
     });
 
 });
